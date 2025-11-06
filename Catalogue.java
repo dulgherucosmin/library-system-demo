535036cfebf79178e55ee2b8d5de17e0edbcd009
NotCosmo <cosmin.dulgheru.alex@gmail.com>
2025-11-06 23:31:44 +0000

    add baseline for the Library interface (catalogue interface)

---
+++ Catalogue.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+interface Catalogue {
+
+    public void addBook(Book book);
+    //add book to library
+
+    public ArrayList <Book> getAvailableBooks();
+    // gets all books that are not currently borrowed
+
+    public ArrayList<PrintedBook> getPrintedBooks();
+    //return a list of just PrintedBooks
+
+    public ArrayList<EBook> getEBooks();
+    //return a list of just EBooks
+
+    public ArrayList<AudioBook> getAudioBooks();
+    //return a list of just AudioBooks
+
+    public Book borrow(String title, String author) ;
+    //checks if a book with the title and author is available
+    //return null if not found
+    //let the user know if not found or just no copies available. 
+
+    public Book borrow(String title, String author, String type) ;
+    //checks if a book of that type with the title and author is available
+    //return null if not found
+    //let the user know if not found or just no copies available. 
+
+    public void returnBook(Book book);
+    //update the book to not borrowed
+    
+}
